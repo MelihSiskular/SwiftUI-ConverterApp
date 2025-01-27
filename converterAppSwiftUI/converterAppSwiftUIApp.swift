da20deecd0d2f9aeaba92b32ee834d3df3d54309
Melih Şişkular <feis_melih@hotmail.com>
2025-01-27 15:13:44 +0700

    Initial Commit

---
+++ converterAppSwiftUI/converterAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  converterAppSwiftUIApp.swift
+//  converterAppSwiftUI
+//
+//  Created by Melih Şişkular on 27.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct converterAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
